08112b396f3f3dcacec2d0e263074fc2a5a2dc43
Albano0 <pedroalbanofernandes12@gmail.com>
2024-06-11 13:22:17 -0300

    adicionando o footer

--- view/perfil.php
+++ view/perfil.php
@@ -51,6 +51,7 @@ $conn = null;
     <link rel="stylesheet" href="../css/reset.css">
     <link rel="stylesheet" href="../css/super.css">
     <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="footer.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
