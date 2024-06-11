5aecf673e9abaee957a66c0870d4e6406e121806
Albano0 <pedroalbanofernandes12@gmail.com>
2024-06-11 13:18:24 -0300

    .

--- view/perfil.php
+++ view/perfil.php
@@ -119,6 +119,18 @@ $conn = null;
             }
         }
     </script>
+
+<footer>
+        <div class="footer-content">
+            <p>&copy; 2024 Sua Empresa. Todos os direitos reservados.</p>
+            <p>
+                Siga-nos:
+                <a href="https://www.facebook.com" target="_blank">Facebook</a> |
+                <a href="https://www.twitter.com" target="_blank">Twitter</a> |
+                <a href="https://www.instagram.com" target="_blank">Instagram</a>
+            </p>
+        </div>
+    </footer>   
 </body>
 
 </html>
