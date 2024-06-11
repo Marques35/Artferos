c5c344e3630040ec913323e9c2f823bcc706fa20
Albano0 <pedroalbanofernandes12@gmail.com>
2024-06-11 13:39:59 -0300

    .

--- view/perfil.php
+++ view/perfil.php
@@ -97,6 +97,8 @@ $conn = null;
         </section>
         <br>
     </main>
+    <?php include '../controller/footer.php'; ?>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script>
         function enableEditing() {
@@ -120,18 +122,6 @@ $conn = null;
             }
         }
     </script>
-
-<footer>
-        <div class="footer-content">
-            <p>&copy; 2024 Sua Empresa. Todos os direitos reservados.</p>
-            <p>
-                Siga-nos:
-                <a href="https://www.facebook.com" target="_blank">Facebook</a> |
-                <a href="https://www.twitter.com" target="_blank">Twitter</a> |
-                <a href="https://www.instagram.com" target="_blank">Instagram</a>
-            </p>
-        </div>
-    </footer>   
 </body>
 
 </html>
