d294e8da5fe5c1fffd4a72f9b6690718f76a4afd
Albano0 <pedroalbanofernandes12@gmail.com>
2024-06-17 10:28:39 -0300

    .

--- controller/footer.php
+++ controller/footer.php
@@ -1,11 +1,11 @@
 <footer>
         <div class="footer-content">
-            <p>&copy; 2024 Sua Empresa. Todos os direitos reservados.</p>
             <p>
                 Siga-nos:
                 <a href="https://www.facebook.com" target="_blank">Facebook</a> |
                 <a href="https://www.twitter.com" target="_blank">Twitter</a> |
-                <a href="https://www.instagram.com" target="_blank">Instagram</a>
+                <a href="https://www.instagram.com" i target="_blank">Instagram</a>
             </p>
+            <p>&copy; 2024 Projeto Minas - Fauna.</p>
         </div>
 </footer> 
--- view/perfil.php
+++ view/perfil.php
@@ -95,7 +95,7 @@ $conn = null;
         </section>
         <br>
     </main>
-    <?php include '../controller/footer.php'; ?>
+    
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script>
@@ -123,6 +123,11 @@ $conn = null;
 
     <script src="mostrarSenha.js"></script>
 </body>
+<footer>
+<br><br>
+   <center><?php include '../controller/footer.php'; ?></center>
+</footer>
+
 
 </html>
 
