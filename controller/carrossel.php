a50c833b3dbc65c9f1e19f6fa8cdede738b6e7b9
Gabriel <gm.marques0605@gmail.com>
2024-06-20 12:35:53 -0300

    tcc atualizado

--- controller/carrossel.php
+++ controller/carrossel.php
@@ -6,13 +6,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="../css/carrossel.css">
   <link rel="stylesheet" href="../css/style.css">
+  <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,0,0">
   <title>Document</title>
 </head>
 
 <body>
   <main>
     <div id="items-wrapper">
-   
         <div id="items">
           <div class="item"><img src="../img/8436721_1.png" /></div>
           <div class="item"><img src="../img/7-71159_jungle-wallpaper-hd.jpg"></div>
